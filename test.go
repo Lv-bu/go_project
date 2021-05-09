f095084ac8b90e9d556234497c86de956cce918f
Lv-bu <875053115@qq.com>
2021-05-09 19:10:59 +0800

    test

--- test.go
+++ test.go
@@ -2,7 +2,7 @@ package main //把test.go文件归属到main,表示test01.go文件所在包是ma
 
 import "fmt" //引入包fmt
 func main() { //main是主函数
-	//输出内容
+	//输出内容：test
 	fmt.Println("hello, My name is Eastmount!")
 	fmt.Println("go" + "lang")
 	fmt.Println("1+1 =", 1+1)
